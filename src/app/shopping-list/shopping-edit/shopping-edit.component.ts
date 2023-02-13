286efe08248244a338fbc0a4eeb8782700559afc
juliock <juliock@tecgraf.puc-rio.br>
2023-02-13 16:56:33 -0300

    Adicionar novos ingredientes em shopping-list sem utilizar  @viewChild

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -8,8 +8,8 @@ import { Ingredient } from '../../shared/ingredient.model';
 })
 export class ShoppingEditComponent implements OnInit{
 
-  @ViewChild('nameInput',{static:false}) nameInputRef: ElementRef;
-  @ViewChild('amountInput',{static:false}) amountInputRef: ElementRef;
+  //@ViewChild('nameInput',{static:false}) nameInputRef: ElementRef;
+  //@ViewChild('amountInput',{static:false}) amountInputRef: ElementRef;
 
   @Output() ingredientAdded = new EventEmitter<Ingredient>();
   constructor() { }
@@ -17,9 +17,11 @@ export class ShoppingEditComponent implements OnInit{
   ngOnInit() {
   }
 
-  onAddItem(){
-    const ingredient = new Ingredient(this.nameInputRef.nativeElement.value, +this.amountInputRef.nativeElement.value)
-    console.log("name = " + this.nameInputRef.nativeElement.value + " amount = " +this.amountInputRef.nativeElement.value)
+  onAddItem(name, amount){
+    //const ingredient = new Ingredient(this.nameInputRef.nativeElement.value, +this.amountInputRef.nativeElement.value)
+    //console.log("name = " + this.nameInputRef.nativeElement.value + " amount = " +this.amountInputRef.nativeElement.value)
+    console.log("name = " + name.value + " amount = " +amount.value)
+    const ingredient = new Ingredient(name.value, amount.value)
     this.ingredientAdded.emit(ingredient);
   }
 }
