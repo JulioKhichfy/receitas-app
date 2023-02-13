9cb5e41d5374a1d8f9ff59008cb0bb3c134c6b53
juliock <juliock@tecgraf.puc-rio.br>
2023-02-13 18:15:09 -0300

    Melhorando a Directive com @HostBinding

--- src/app/directives/mouse-over-change-color.directive.ts
+++ src/app/directives/mouse-over-change-color.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, ElementRef, HostListener, OnInit, Renderer2 } from '@angular/core';
+import { Directive, ElementRef, HostBinding, HostListener, OnInit, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[appMouseOverChangeColor]'
@@ -6,6 +6,8 @@ import { Directive, ElementRef, HostListener, OnInit, Renderer2 } from '@angular
 
 
 export class MouseOverChangeColorDirective implements OnInit{
+
+  @HostBinding('style.backgroundColor') backgroundColor:string;
 
   constructor(private elRef: ElementRef ,private renderer: Renderer2) { }
   
@@ -15,12 +17,14 @@ export class MouseOverChangeColorDirective implements OnInit{
   }
 
   @HostListener('mouseenter') mouseover(eventData: Event){
-    this.renderer.setStyle(this.elRef.nativeElement, 'background-color','blue');
+    //this.renderer.setStyle(this.elRef.nativeElement, 'background-color','blue');
+    this.backgroundColor = 'blue';
     this.renderer.setStyle(this.elRef.nativeElement, 'color','white');
   }
 
   @HostListener('mouseleave') mouseleave(eventData: Event){
-    this.renderer.setStyle(this.elRef.nativeElement, 'background-color','transparent');
+    //this.renderer.setStyle(this.elRef.nativeElement, 'background-color','transparent');
+    this.backgroundColor = 'transparent';
     this.renderer.setStyle(this.elRef.nativeElement, 'color','red');
   }
 
