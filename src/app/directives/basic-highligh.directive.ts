d7689d5f31fd01268494e20a3975e80f26ac2c40
juliock <juliock@tecgraf.puc-rio.br>
2023-02-13 17:53:17 -0300

    Melhorando a directive com Renderer2

--- src/app/directives/basic-highligh.directive.ts
+++ src/app/directives/basic-highligh.directive.ts
@@ -8,7 +8,13 @@ export class BasicHighlighDirective implements OnInit{
   constructor(private elementRef:ElementRef) {
 
   }
-  
+
+  //nao eh boa pratica acessar diretamente seus elementos
+  //como estavamos fazendo:
+  //this.elementRef.nativeElement.style.backgroundColor = 'green';
+  //this.elementRef.nativeElement.style.color = 'white';
+
+  //por isso vamos acessar de outra forma na diretica better-highlight
   ngOnInit(): void {
     this.elementRef.nativeElement.style.backgroundColor = 'green';
     this.elementRef.nativeElement.style.color = 'white';
